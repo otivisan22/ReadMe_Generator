d6b14cd5aa77019b6968c831dac56ad3e13c0d0b
Otilia Visan <otilia.visan22@gmail.com>
2021-04-30 23:10:22 +0100

    declared the licenses

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
+const fs = require ("fs");
+const generateMarkdown = require ("./utils/generateMarkdown.js");
+
 
 //licenses
 
-const apache= "Licensed under the [Apache License] (https://www.apache.org/licenses/LICENSE-2.0).";
+const apache = "Licensed under the [Apache License] (https://www.apache.org/licenses/LICENSE-2.0).";
 const mit =  "Licensed under the [mit License](https://opensource.org/licenses/MIT).";
 const gnu = "Licensed under the [Gnu GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html).";
 const bsd = "Licensed under the [The 3-Clause BSD] (https://opensource.org/licenses/BSD-3-Clause).";
@@ -53,10 +56,23 @@ const questions = [
     name: "tests";
     message: "How would you run tests on this project?"
   },
+
+  {
+    type: "list";
+    name: "contributors";
+    message: "Would you like other developers to work on this project?"
+    choices: [
+      "Yes",
+      "No"
+    ]
+  },
 ];
 
 // TODO: Create a function to write README file
-const writeToFile = (fileName, data) => {};
+// function err
+const writeToFile = (fileName, data) => {
+  
+};
 
 // TODO: Create a function to initialize app
 const init = () => {};
