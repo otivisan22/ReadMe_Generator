faffa34721f71f1699acc2d5eed0fd3f73b390c8
Otilia Visan <otilia.visan22@gmail.com>
2021-04-30 23:02:09 +0100

    declared the questions

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
+//licenses
+
+const apache= "Licensed under the [Apache License] (https://www.apache.org/licenses/LICENSE-2.0).";
+const mit =  "Licensed under the [mit License](https://opensource.org/licenses/MIT).";
+const gnu = "Licensed under the [Gnu GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html).";
+const bsd = "Licensed under the [The 3-Clause BSD] (https://opensource.org/licenses/BSD-3-Clause).";
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input";
+    name: "title";
+    message: "What's the name of the project?"
+  },
+
+  {
+    type: "list";
+    name: "license";
+    message: "What type of licenses would you like?"
+    choices: [
+      "APACHE_2.0",
+      "MIT",
+      "GPL_3.0",
+      "BSD_3", 
+      "None"]
+  },
+
+  {
+    type: "input";
+    name: "description";
+    message: "Can you resume your project?"
+  },
+
+  {
+    type: "input";
+    name: "installation";
+    message: "What do you need to install so that the application will work?"
+  },
+
+  {
+    type: "input";
+    name: "usage";
+    message: "Which are the steps for using your application?"
+  },
+
+  {
+    type: "input";
+    name: "tests";
+    message: "How would you run tests on this project?"
+  },
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
