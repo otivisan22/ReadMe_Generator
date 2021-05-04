06508885459c6d08974ef27d984ba893b293efdb
Otilia Visan <otilia.visan22@gmail.com>
2021-05-04 16:53:04 +0100

    fixed the error

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
-const fs = require ("fs");
-const generateMarkdown = require ("./utils/generateMarkdown.js");
-
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 //licenses
 
-const apache = "Licensed under the [Apache License] (https://www.apache.org/licenses/LICENSE-2.0).";
-const mit =  "Licensed under the [mit License](https://opensource.org/licenses/MIT).";
-const gnu = "Licensed under the [Gnu GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html).";
-const bsd = "Licensed under the [The 3-Clause BSD] (https://opensource.org/licenses/BSD-3-Clause).";
-
+const apache =
+  "Licensed under the [Apache License] (https://www.apache.org/licenses/LICENSE-2.0).";
+const mit =
+  "Licensed under the [mit License](https://opensource.org/licenses/MIT).";
+const gnu =
+  "Licensed under the [Gnu GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html).";
+const bsd =
+  "Licensed under the [The 3-Clause BSD] (https://opensource.org/licenses/BSD-3-Clause).";
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -25,19 +27,13 @@ const questions = [
     type: "list",
     name: "license",
     message: "What type of licenses would you like?",
-    choices: [
-      "APACHE_2.0",
-      "MIT",
-      "GPL_3.0",
-      "BSD_3", 
-      "None"]
+    choices: ["APACHE_2.0", "MIT", "GPL_3.0", "BSD_3", "None"],
   },
 
   {
     type: "input",
     name: "description",
     message: "Can you resume your project?",
-
   },
 
   {
@@ -62,41 +58,33 @@ const questions = [
     type: "list",
     name: "contributors",
     message: "Would you like other developers to work on this project?",
-    choices: [
-      "Yes",
-      "No"
-    ]
+    choices: ["Yes", "No"],
   },
 ];
-
-// async function in order to insert all the data 
 
 // TODO: Create a function to write README file
 // function err
 const writeToFile = (data) => {
-const callback(err) => {
-  if(err){
-    console.log(err)
-  } else{
-    console.log ("Generated ReadMe has be created successfully")
-  }
-}
+  const callback = (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Generated ReadMe has be created successfully");
+    }
+  };
 
-fs.writeFile(generated_readme.md, data, callback);
-
-  
+  fs.writeFile(generated_readme.md, data, callback);
 };
 
 // TODO: Create a function to initialize app
 const init = async () => {
-const answers = await inquirer.prompt(questions);
+  const answers = await inquirer.prompt(questions);
 
-console.log(answers);
+  console.log(answers);
 
- const generatedMarkdown = generateMarkdown(answers);
+  const generatedMarkdown = generateMarkdown(answers);
 
- writeToFile(generated_readme.md);
-
+  writeToFile(generated_readme.md);
 };
 
 // Function call to initialize app
